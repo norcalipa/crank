44b812a8e860a274ac6552d68d6d5cce84038a81
= <=>
2023-10-30 16:42:48 -0700

    Created initial crank data model

---
+++ crank/models.py
@@ -0,0 +1,57 @@
+
+from django.db import models
+
+
+class OrganizationType(models.Model):
+    COMPANY = "C"
+    NON_PROFIT = "N"
+    TYPE = (
+        (COMPANY, "Company (for profit)"),
+        (NON_PROFIT, "Non-Profit Organization"),
+    )
+
+    def __str__(self):
+        return self.name
+
+    short_name = models.CharField(max_length=1, default=COMPANY)
+    name = models.CharField(max_length=20)
+    gives_ratings = models.BooleanField(default=False)
+
+
+class Organization(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=100, unique=True)
+    type = models.ForeignKey(OrganizationType, on_delete=models.RESTRICT)
+
+
+class ScoreType(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=100)
+
+
+class ScoreAlgorithm(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=100)
+
+
+class ScoreAlgorithmWeights(models.Model):
+    type = models.ForeignKey(ScoreType, on_delete=models.CASCADE)
+    algorithm = models.ForeignKey(ScoreAlgorithm, on_delete=models.CASCADE)
+    weight = models.FloatField(default=1.0)
+
+
+class Score(models.Model):
+    def __str__(self):
+        return str(self.score)
+
+    source = models.ForeignKey(Organization, on_delete=models.RESTRICT, related_name="source_organization")
+    target = models.ForeignKey(Organization, on_delete=models.RESTRICT, related_name="target_organization")
+    score = models.FloatField(default=0.0)
+    low_threshold = models.FloatField(default=0.0)
+    high_threshold = models.FloatField(default=5.0)
