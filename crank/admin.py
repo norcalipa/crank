dc3a8b885ada9735620d0652dcc82909a7d1371c
= <=>
2023-11-09 13:56:08 -0800

    Admin Tweaks
    - Adding some filters and changing columns to allow for more efficient editing and searching
    - Changing the styling of the nav a tiny bit

--- crank/admin.py
+++ crank/admin.py
@@ -7,16 +7,40 @@ class ScoreInline(admin.TabularInline):
     model = Score
     fk_name = 'target'
     fields = ['status', 'type', 'source', 'score']
+    extra = 1
+
+
+class ScoreAdmin(admin.ModelAdmin):
+    model = Score
+    list_display = ['target', 'type', 'score', 'source']
+    list_editable = ['score', 'type', 'source']
+    list_filter = ['status', 'type']
+    search_fields = ['target__name']
+
+
+class ScoreTypeAdmin(admin.ModelAdmin):
+    model = ScoreType
+    list_display = ['status', 'name']
+    list_filter = ['status']
+
+
+class ScoreAlgorithmAdmin(admin.ModelAdmin):
+    model = ScoreAlgorithm
+    list_display = ['name', 'description_content']
+    list_filter = ['status']
 
 
 class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'type', 'url', 'gives_ratings', 'public', 'funding_round', 'rto_policy']
+    list_filter = ['status', 'type']
+    list_editable = ['type', 'funding_round', 'rto_policy']
+    search_fields = ['name']
     model = Organization
-
     inlines = [ScoreInline]
 
 
 admin.site.register(Organization, OrganizationAdmin)
-admin.site.register(ScoreType)
-admin.site.register(ScoreAlgorithm)
+admin.site.register(ScoreType, ScoreTypeAdmin)
+admin.site.register(ScoreAlgorithm, ScoreAlgorithmAdmin)
 admin.site.register(ScoreAlgorithmWeight)
-admin.site.register(Score)
+admin.site.register(Score, ScoreAdmin)
