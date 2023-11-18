758bf42a83cf879add92e982c1bb47c76bb474c0
= <=>
2023-11-17 16:46:43 -0800

    Admin Tweaks, Data, and Score Unique Constraint
    - Changed inline editing of scores to remove all of the extraneous icons to make data entry easier
    - Added some more companies
    - Added a unique constraint on scores to ensure there is not more than one active score of a given type for a given company and ratings source (there can be an unlimited number of inactive scores)

--- crank/models/score.py
+++ crank/models/score.py
@@ -1,4 +1,4 @@
-from django.db.models import Avg
+from django.db.models import Avg, Index, Q, UniqueConstraint
 
 from crank.models.organization import Organization
 from django.db import models
@@ -21,7 +21,7 @@ class ScoreAlgorithm(TimeStampedModel, ActivatorModel):
 
 
 class ScoreAlgorithmWeight(TimeStampedModel, ActivatorModel):
-    type = models.ForeignKey(ScoreType, on_delete=models.CASCADE)
+    type = models.ForeignKey(ScoreType, on_delete=models.CASCADE, limit_choices_to={"status": 1})
     algorithm = models.ForeignKey(ScoreAlgorithm, on_delete=models.CASCADE)
     weight = models.FloatField(default=1.0)
 
@@ -30,9 +30,18 @@ class Score(TimeStampedModel, ActivatorModel):
     def __str__(self):
         return "{}: {} [{}]={} ".format(self.target.name, self.type.name, self.source.name, self.score)
 
-    type = models.ForeignKey(ScoreType, on_delete=models.CASCADE, default=None)
-    source = models.ForeignKey(Organization, on_delete=models.RESTRICT, related_name="scores_given")
+    type = models.ForeignKey(ScoreType, on_delete=models.CASCADE, default=None,
+                             limit_choices_to={"status": 1})
+    source = models.ForeignKey(Organization, on_delete=models.RESTRICT, related_name="scores_given",
+                               limit_choices_to={"status": 1, "gives_ratings": 1})
     target = models.ForeignKey(Organization, on_delete=models.RESTRICT, related_name="scores")
     score = models.FloatField(default=0.0)
     low_threshold = models.FloatField(default=0.0)
     high_threshold = models.FloatField(default=5.0)
+
+    class Meta:
+        constraints = [
+            # this constraint ensures there is only one active score for a given type, source, and target
+            UniqueConstraint(name="unique_score_type_source_target_status", fields=["type", "source", "target", "status"],
+                             condition=Q(status=1), violation_error_message="There is already an active score of this type")
+        ]
