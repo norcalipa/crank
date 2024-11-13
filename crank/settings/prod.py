1449469be046594cba1cc610a4d62c72cf875549
Isaac Adams <81994232+norcalipa@users.noreply.github.com>
2024-11-12 16:05:52 -0800

    Revert "Updating settings for multithreading"

--- crank/settings/prod.py
+++ crank/settings/prod.py
@@ -20,12 +20,8 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
-        'CONN_MAX_AGE': 0,  # Use 0 for connection pooling
-        'OPTIONS': {
-            'MAX_CONNS': 8,  # Maximum number of connections in the pool
-            'REUSE_CONNS': 4,  # Number of connections to reuse
-        },
+        'PASSWORD': os.environ.get('DB_PASS')
+#        'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
     }
 }
 ALLOWED_HOSTS = ['*']
