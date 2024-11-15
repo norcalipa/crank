b1428328f47c5605050b01a80f2eba0d1bad4b11
= <=>
2024-11-14 20:01:45 -0800

    Adding additional caching, moving cache TTL to ENV

--- crank/views/index.py
+++ crank/views/index.py
@@ -13,7 +13,6 @@ from crank.forms.organization_filter import OrganizationFilterForm
 
 
 class IndexView(generic.ListView):
-    algorithm_cache = {}
     template_name = "crank/index.html"
     context_object_name = "top_organization_list"
     paginate_by = 15
@@ -35,14 +34,20 @@ class IndexView(generic.ListView):
     def _check_algorithm_id(self):
         if not self.algorithm:
             if 'algorithm_id' in self.kwargs:
-                self.algorithm_id = self.kwargs['algorithm_id']
+                self.algorithm_id = int(self.kwargs['algorithm_id'])
 
-            self.algorithm = self.algorithms.filter(id=self.algorithm_id).first()
-            if self.algorithm:
-                return
-            else:
-                self.algorithm_id = DEFAULT_ALGORITHM_ID
-                self.request.session["algorithm_id"] = self.algorithm_id
+            if self.algorithm_id:
+                self.algorithm = self.algorithms.filter(id=int(self.algorithm_id)).first()
+                if self.algorithm:
+                    return
+
+            self.algorithm_id = DEFAULT_ALGORITHM_ID
+            self.request.session["algorithm_id"] = self.algorithm_id
+            try:
+                if not self.algorithm:
+                    self.algorithm = self.algorithms.filter(id=self.algorithm_id).first()
+            except ScoreAlgorithm.DoesNotExist:
+                pass  # we will handle empty algorithms by returning an empty object list
 
     def post(self, request, *args, **kwargs):
         self.kwargs = kwargs
@@ -53,7 +58,8 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         # if no algorithm_id in the URL, check for one in the session
         if not self.algorithm_id:
-            self.algorithm_id = self.request.session.get("algorithm_id")
+            self.algorithm_id = int(self.request.session.get(
+                "algorithm_id")) if "algorithm_id" in self.request.session else DEFAULT_ALGORITHM_ID
 
         self.accelerated_vesting = self.request.session.get('accelerated_vesting', False)
 
@@ -106,7 +112,6 @@ class IndexView(generic.ListView):
         self.object_list = cache.get_or_set(cache_key, fetch_results, timeout=settings.CACHE_MIDDLEWARE_SECONDS)
         return self.object_list
 
-
     def get_context_data(self, **kwargs):
         if kwargs is None:
             kwargs = {}
@@ -123,13 +128,15 @@ class IndexView(generic.ListView):
     def get_algorithm_details(self):
         if self.error:
             return None
-        self.algorithm_id = self.request.session.get("algorithm_id")
 
         self._check_algorithm_id()
-        if not hasattr(self.algorithm, 'html_description_content'):
-            file_path = os.path.join(CONTENT_DIR, self.algorithm.description_content)
-            with open(file_path, 'r') as file:
-                md_content = file.read()
-            html_content = markdown.markdown(md_content)
-            self.algorithm.html_description_content = html_content
+        if self.algorithm and not hasattr(self.algorithm, 'html_description_content'):
+            def get_html_content():
+                file_path = os.path.join(CONTENT_DIR, self.algorithm.description_content)
+                with open(file_path, 'r') as file:
+                    md_content = file.read()
+                return markdown.markdown(md_content)
+
+            self.algorithm.html_description_content = cache.get_or_set(
+                f'algorithm_{self.algorithm_id}_description', get_html_content(), timeout=settings.CACHE_MIDDLEWARE_SECONDS)
         return self.algorithm
